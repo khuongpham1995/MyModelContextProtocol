bfb663de59be8c6730ece8c9536ffeb1a7b3122d
Kyle Pham <kylepham184@gmail.com>
2025-05-23 10:50:58 +0700

    Update ToolExtension.cs

--- src/McpServer/McpServer.Function/McpTools/ToolExtension.cs
+++ src/McpServer/McpServer.Function/McpTools/ToolExtension.cs
@@ -8,7 +8,11 @@ public static class ToolExtension
     public static void RegisterMcpTools(this FunctionsApplicationBuilder builder)
     {
         builder
-            .ConfigureMcpTool(CommonConstant.AskBackendDocumentToolName)
+            .ConfigureMcpTool(CommonConstant.AskBackendStructureDocumentToolName)
+            .WithProperty(CommonConstant.QuestionPropertyName, CommonConstant.PropertyTypeString, CommonConstant.QuestionPropertyDescription);
+
+            builder
+            .ConfigureMcpTool(CommonConstant.AskBackendTestsDocumentToolName)
             .WithProperty(CommonConstant.QuestionPropertyName, CommonConstant.PropertyTypeString, CommonConstant.QuestionPropertyDescription);
     }
 }
